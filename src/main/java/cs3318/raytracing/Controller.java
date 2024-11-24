38b7cf7ab5721f05b0339909079801c528059668
SequeI <asiek@redhat.com>
2024-11-24 19:12:17 +0000

    adding proper error logging + try/catch block

--- src/main/java/cs3318/raytracing/Controller.java
+++ src/main/java/cs3318/raytracing/Controller.java
@@ -3,13 +3,16 @@ package cs3318.raytracing;
 import javafx.event.ActionEvent;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import java.util.logging.Logger;
 
 
 public class Controller {
     public ImageView renderedImage;
     private Stage stage;
     private Driver sceneToRender;
-    boolean finished = false;
+    private Optional<Boolean> finished = Optional.empty();
+
+    Logger logger = Logger.getLogger(Controller.class.getName());
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -17,18 +20,21 @@ public class Controller {
 
     public void run() {
         long time = System.currentTimeMillis();
-        for (int j = 0; j < sceneToRender.height; j += 1) {
-            for (int i = 0; i < sceneToRender.width; i += 1) {
-                sceneToRender.renderPixel(i, j);
+        try {
+            for (int j = 0; j < sceneToRender.height; j += 1) {
+                for (int i = 0; i < sceneToRender.width; i += 1) {
+                    sceneToRender.renderPixel(i, j);
+                }
             }
+            renderedImage.setImage(sceneToRender.getRenderedImage());
+            finished = Optional.of(true);
+            logger.info("Image has been rendered within " + (System.currentTimeMillis() - time) + " ms");
+        } catch (Exception e) {
+            logger.severe("Error occured during rendering: " + e.getMessage());
         }
-        renderedImage.setImage(sceneToRender.getRenderedImage());
-        time = System.currentTimeMillis() - time;
-        System.err.println("Rendered in "+(time/60000)+":"+((time%60000)*0.001));
-        finished = true;
     }
-
-
+ 
+    // TODO to change into API, instead of rendering from config within a txt file in driver.java
     public void startRayTrace(ActionEvent actionEvent) {
         sceneToRender = new Driver((int) renderedImage.getFitWidth(),
                                    (int) renderedImage.getFitHeight(),
